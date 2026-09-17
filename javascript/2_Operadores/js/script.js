fe5a7f8f7beca081da7333aed052769a81d6fcdb
Pedro Antônio <pedro.riber201726@gmail.com>
2026-09-16 22:31:14 -0300

    Javascript

---
+++ javascript/2_Operadores/js/script.js
@@ -0,0 +1,24 @@
+// 1 - number
+console.log(typeof 2);
+console.log(typeof 5.14);
+console.log(typeof -127);
+
+// 2 Ops. aritimeticas
+
+console.log(2+4);
+console.log(10-5);
+console.log(5*4);
+console.log(4/2);
+
+console.log(5+(4*2));
+
+
+// 3 specal number
+
+console.log(typeof Infinity);
+
+console.log(typeof -Infinity);
+
+console.log(12 * 'asd');
+
+console.log(typeof NaN);
