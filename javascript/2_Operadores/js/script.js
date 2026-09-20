71f6f7c0eeeeb42abc9fb0e02e09b38450d98796
Pedro Antônio <pedro.riber201726@gmail.com>
2026-09-19 22:47:10 -0300

    a

--- javascript/2_Operadores/js/script.js
+++ javascript/2_Operadores/js/script.js
@@ -22,3 +22,57 @@ console.log(typeof -Infinity);
 console.log(12 * 'asd');
 
 console.log(typeof NaN);
+
+// 4 - strings
+
+console.log("Um texto");
+console.log(`Mais um texto`);
+console.log(`13`);
+
+console.log(typeof "Um texto");
+console.log(typeof `Mais um texto`);
+
+// 5 - Simbolos especiais em string
+
+console.log("Testando a \n quebra de linha");
+
+console.log("Espaçamento \t de tab");
+
+// 6 - Concatenação
+
+console.log("Oi," + "tudo" + " bem?");
+
+console.log(`Testando `+`com `+`crase!`);  
+
+// 7 - template strings
+console.log(`A soma entre 2+2 é: ${2+2}`);
+
+console.log(`Podemos executar qualquer coisa aqui ${console.log("teste")}`);
+
+// 8 - boolean
+
+console.log(true);
+
+console.log(5 > 20);
+
+console.log(30 > 10);
+
+console.log( typeof false);
+
+// 9 - comparações
+console.log(5 <= 5);
+
+console.log(5 < 5);
+
+console.log(10 == 10); 
+console.log(9 == 10);
+
+console.log(10 != 9 )
+console.log(9 != 10)
+
+// 10 - identico
+console.log(9 == "9")
+console.log(9 === "9")
+
+console.log(9 != "9")
+console.log(9 !== "9")
